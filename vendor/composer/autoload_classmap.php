c2de8872c617d379886e144d11390bfacad77bea
stiofan <info@wpgeodirectory.com>
2022-11-15 10:07:40 +0000

    SD updated

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,6 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Composer\\InstalledVersions' => $vendorDir . '/composer/InstalledVersions.php',
     'WP_Super_Duper' => $vendorDir . '/ayecode/wp-super-duper/wp-super-duper.php',
 );
